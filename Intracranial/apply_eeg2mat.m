1d4cd801d0dc2468ebeadfeb459821dff50858aa
kuz <ilya.kuzovkin@gmail.com>
2015-08-17 18:50:56 +0300

    Convert .eeg files to .mat

---
+++ Intracranial/apply_eeg2mat.m
@@ -0,0 +1,24 @@
+%
+% Convert .eeg files to .mat matrices
+% Elan (http://elan.lyon.inserm.fr/) should be installed so that eeg2mat script in the Matlab path
+% See http://elan.lyon.inserm.fr/?q=eeg2mat for more information
+%
+
+% add elan scipts to the path
+addpath('/home/kuz/Software/Elan/misc/matlab')
+
+% compose a list of .eeg file
+eegfiles = dir('../../Data/Raw_data/*.eeg');
+
+% process .eeg files one by one
+for i = 1:length(eegfiles)
+
+    % display progress
+    disp(['Processing ' eegfiles(i).name ' ' num2str(i) '/' num2str(length(eegfiles))])
+
+    % convert an .eeg file
+    [m_data, m_events, v_label_selected, s_fs, s_nb_samples_all, s_nb_channel_all, ...
+     v_label_all, v_channel_type_all, v_channel_unit_all, str_ori_file1, str_ori_file2] = ...
+     eeg2mat(['../../Data/Raw_data/' eegfiles(i).name], 1, 'all', 'all', 'save', ['../../Data/Raw_data/' eegfiles(i).name '.mat']);
+
+end
