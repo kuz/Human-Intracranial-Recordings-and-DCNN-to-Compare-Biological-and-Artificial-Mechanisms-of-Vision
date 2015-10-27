99a455df80811957cf784d75ffa74e1c1364808e
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2015-10-27 16:13:35 +0200

    (why synthetic data is so classifiable even when it should not be?)
    Former-commit-id: 6fbf350e2ef53d53e4fb3d8144fa0e2d88403e34

--- Collusion/map_layers_to_probes.py
+++ Collusion/map_layers_to_probes.py
@@ -18,69 +18,43 @@ import argparse
 # read in command line arguments
 parser = argparse.ArgumentParser(description='Train all linear models for a given subject.')
 parser.add_argument('-i', '--sid', dest='sid', type=int, required=True, help='Subject ID')
+parset.add_argument('-f', '--featureset', dest='featureset', type=str, required=True, help='Directory under Processed/')
 args = parser.parse_args()
 sid = int(args.sid)
+featureset = str(args.featureset)
 
 # parameters
 #ncores = multiprocessing.cpu_count()
 ncores = 8
 print "Working with %d CPUs" % ncores
 
-def predict_from_layer(layer_activations, layer, subject, subject_id, nsubjects):
-    """
-    Takes one layer and predicts all probe activations from that layer
-    """
+# train linear model to predict probe [pid] response from [layer]
+# activations, measure the prediction performace on the test set
+# of stimuli
+def predict_from_layer(subject_name, layer, pid,
+                       train_layer_activity, assign_layer_activity,
+                       train_probe_responses, assign_probe_responses):
 
-    nstims = subject['data'].shape[0]
-    nprobes = subject['data'].shape[1]
+    # train a model
+    clf = linear_model.Lasso(alpha = 0.1, max_iter=10000)
+    clf.fit(train_layer_activity, train_probe_responses)
+
+    # attempt prediction on the assignment set
+    true = assign_probe_responses
+    predicted = clf.predict(assign_layer_activity)
+
+    # store the correlation coefficient
+    r, pval = pearsonr(true, predicted)
+
+    # drop not significant results
+    if pval > 0.05:
+        r = 0.0
 
     # display progress
-    print 'Training models for subject %s (%d/%d), layer %s' % (subject['name'], subject_id, nsubjects, layer)
+    print 'Fitted %s: %s to probe %d -- %f (p=%f)' % (subject_name, layer, pid, r, pval)
 
-    # place to store coefficients
-    r_coefs = np.zeros(nprobes)
+    return (layer, pid, r)
 
-    # pick 70% of stimuli for training and 30% for layer assignment
-    # TODO: should this be before the for cycle
-    # TODO: replace with cross-validation
-    train_idx = np.random.choice(range(nstims), size=int(nstims * 0.7), replace=False)
-    assign_idx = list(set(range(nstims)) - set(train_idx))
-
-    # create the dataset
-    layer_activity = np.zeros((nstims, layer_activations.shape[1]))
-    probe_responses = np.zeros((nstims, nprobes))
-    for sid_brain, stimulus in enumerate(subject['stimseq']):
-        sid_dnn = dnn_stimuli.index(stimulus)
-        layer_activity[sid_brain, :] = layer_activations[sid_dnn, :]
-        probe_responses = subject['data']
-
-    # split the dataset
-    train_layer_activity = layer_activity[train_idx, :]
-    train_probe_responses = probe_responses[train_idx, :]
-    assign_layer_activity = layer_activity[assign_idx, :]
-    assign_probe_responses = probe_responses[assign_idx, :]
-
-    # build a linear predictor for each probe
-    for pid in range(len(subject['probes']['probe_ids'])):
-
-        print '  %s, %s, %d' % (subject['name'], layer, pid)
-
-        # train a model
-        clf = linear_model.Lasso(alpha = 0.1, max_iter=10000)
-        clf.fit(train_layer_activity, train_probe_responses[:, pid])
-
-        # attempt prediction on the assignment set
-        true = assign_probe_responses[:, pid]
-        predicted = clf.predict(assign_layer_activity)
-
-        # store the correlation coefficient
-        r, pval = pearsonr(true, predicted)
-        if pval <= 0.05:
-            r_coefs[pid] = r
-        else:
-            r_coefs[pid] = 0.0
-
-    return (subject['name'], layer, r_coefs)
 
 # prepare lists of probe coordinates for each layer
 print 'Loading list of layers...'
@@ -101,17 +75,14 @@ dnn_stimuli = np.loadtxt('../../Data/DNN/imagesdone.txt', dtype={'names': ('stim
 dnn_stimuli = [x[0].split('.')[0] for x in dnn_stimuli]
 
 # read list of subjects
-listing = os.listdir('../../Data/Intracranial/Processed/maxamp/')
-
-# grid of (subject, layer) pairs to compute
-subject_layer_grid = []
+listing = os.listdir('../../Data/Intracranial/Processed/%s/' % featureset)
 
 # load brain data
 sfile = listing[sid]
 print '  Loading %s...' % sfile
 
 # load the matlab structure 
-s = sio.loadmat('../../Data/Intracranial/Processed/maxamp/%s' % sfile)
+s = sio.loadmat('../../Data/Intracranial/Processed/%s/%s' % (featureset, sfile))
 subject = {}
 subject['stimseq'] = [x[0][0] for x in s['s']['stimseq'][0][0]]
 subject['stimgroups'] = [x[0] for x in s['s']['stimgroups'][0][0]]
@@ -122,15 +93,60 @@ subject['probes']['mni'] = s['s']['probes'][0][0][0][0][2]
 subject['data'] = s['s']['data'][0][0]
 subject['name'] = s['s']['name'][0][0][0]
 
+# for convenience
+nstims = subject['data'].shape[0]
+nprobes = subject['data'].shape[1]
+
+# pick 70% of stimuli for training and 30% for layer assignment
+# TODO: should this be before the for cycle
+# TODO: replace with cross-validation
+train_idx = np.random.choice(range(nstims), size=int(nstims * 0.7), replace=False)
+assign_idx = list(set(range(nstims)) - set(train_idx))
+
+# create the dataset: for each layer we'll have DNN activations as features
+# and probe response as the target value to predict
+layer_activity = {}
 for layer in layers:
-    subject_layer_grid.append((sid, layer))
+    layer_activity[layer] = np.zeros((nstims, activations[layer].shape[1]))
+    probe_responses = np.zeros((nstims, nprobes))
+    for sid_brain, stimulus in enumerate(subject['stimseq']):
+        sid_dnn = dnn_stimuli.index(stimulus)
+        layer_activity[layer][sid_brain, :] = activations[layer][sid_dnn, :]
+        probe_responses = subject['data']
+
+# grid of (subject, layer, pribe) triples to compute in parallel
+#parallel_grid = []
+#for layer in layers:
+#    for pid in range(len(subject['probes']['probe_ids'])):
+#        parallel_grid.append((layer, pid))
+
+parallel_grid = []
+for layer in [fc7']:
+    for pid in range(80, 135):
+        parallel_grid.append((layer, pid))
+
+# for each (layer, probe) combination train a linear model to predict the probe response from the layer activations
+results = Parallel(n_jobs=ncores)(delayed(predict_from_layer)(subject['name'], layer, pid,
+                                                             layer_activity[layer][train_idx, :], layer_activity[layer][assign_idx, :],
+                                                             probe_responses[train_idx, pid], probe_responses[assign_idx, pid])
+                                  for (layer, pid) in parallel_grid)
+
+
+print 'Done for now'
+exit()
+
+# place to store coefficients
+r_coefs = np.zeros(nprobes)
+
+
+
 
 # for each probe, layer combination train a linear model to predict the probe response from the layer activations
 print 'Training linear models...'
 start = time.time()
 results = Parallel(n_jobs=ncores)(delayed(predict_from_layer)
-                                 (activations[layer], layer, subject, subject_id, 1)
-                                 for (subject_id, layer) in subject_layer_grid)
+                                 (activations[layer], layer, subject, subject_id, len(listing), dnn_stimuli)
+                                 for (subject_id, layer) in parallel_grid)
 print 'Training the models took', time.time() - start
 
 # aggregate results and store to files
