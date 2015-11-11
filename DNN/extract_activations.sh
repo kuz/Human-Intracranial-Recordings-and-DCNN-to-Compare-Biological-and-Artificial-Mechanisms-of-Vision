9afa3f4c91d9091391d352e6fb44951328f0d9e6
kuz <ilya.kuzovkin@gmail.com>
2015-11-11 15:20:22 +0200

    Name activation directories according to the model they are extracted from
    Former-commit-id: e0853d0e841ac953ca341a13934b76bf5391fe79

--- DNN/extract_activations.sh
+++ DNN/extract_activations.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 CAFFEHOME=$HOME"/Software/Caffe"
+LMDB="lmdb.empty"
+NUMPY="numpy.empty"
+MODEL="model/bvlc_empty_caffenet.caffemodel"
 
 echo "Removing previously extracted activations ..."
-rm -rf activations/lmdb/*
-rm -rf activations/numpy/*
+rm -rf activations/$LMDB/*
+rm -rf activations/$NUMPY/*
 
 layers=("conv1" "conv2" "conv3" "conv4" "conv5" "fc6" "fc7" "fc8")
 #layers=("fc8")
@@ -16,12 +19,11 @@ do
 	echo " "
 	echo "Extracting activations from layer $layer ..."
 	echo "--------------------------------------------"
-	$CAFFEHOME/build/tools/extract_features.bin model/bvlc_empty_caffenet.caffemodel model/extractor.prototxt \
-											$layer activations/lmdb/$layer 419 lmdb GPU 0
+	$CAFFEHOME/build/tools/extract_features.bin $MODEL model/extractor.prototxt $layer activations/$LMDB/$layer 419 lmdb GPU 0
 
 	# convert lmdb into numpy
-	mkdir activations/numpy/$layer
-	python lmdb2numpy.py activations/lmdb/$layer
+	mkdir activations/$NUMPY/$layer
+	python lmdb2numpy.py activations/$LMDB/$layer
 
 done
 
