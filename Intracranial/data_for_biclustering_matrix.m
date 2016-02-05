de68fb881069d4feda1706693ca79d6a22e93f09
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-02-05 18:19:19 +0200

    Ventral Stream gamma matrix for biclustering
    Former-commit-id: 563ddda3e3a13ce29e30240d5017bb7aca68054d

--- Intracranial/data_for_biclustering_matrix.m
+++ Intracranial/data_for_biclustering_matrix.m
@@ -3,8 +3,13 @@
 %
 
 
+%% Imports
+addpath('lib/mni2name')
+
+
 %% Parameters
-featureset = 'meandelta';
+featureset = 'meangamma_ventral_w250_10hz';
+talareich_level = 5;
 
 
 %% List of subject for whom we have the mapping
@@ -15,10 +20,9 @@ listing = dir(['../../Data/Intracranial/Processed/' featureset '/*.mat']);
 
 % for each subject
 stim_x_el = [];
-subjects = {};
+labels = {};
 for fid = 1:length(listing)
-    [pathstr, name, ext] = fileparts(listing(fid).name);
-    subject = name;
+    [pathstr, subject, ext] = fileparts(listing(fid).name);
     
     % display progress
     disp(['Processing ' num2str(fid) '/' num2str(length(listing)) ': ' subject '...'])
@@ -28,10 +32,18 @@ for fid = 1:length(listing)
     
     % append data to result matrix
     stim_x_el = [stim_x_el s.data];
-    subjects = [subjects repmat({subject}, 1, size(s.data, 2))];
+    
+    % labels A: subject names
+    %labels = [labels repmat({subject}, 1, size(s.data, 2))];
+    
+    % labels B: area name
+    s.probes.mni(isnan(s.probes.mni)) = 0;
+    [~, areas] = mni2name(s.probes.mni);
+    labels = [labels areas(:, talareich_level)'];
+    
     
     % clear workspace
-    clearvars -except listing featureset stim_x_el subjects
+    clearvars -except listing featureset stim_x_el labels talareich_level
     
 end
 
@@ -39,6 +51,6 @@ end
 %% Store the data
 filename = ['../../Outcome/Biclustering matrix/' featureset '.csv'];
 fid = fopen(filename, 'w');
-fprintf(fid, [strjoin(subjects, '\t') '\n']);
+fprintf(fid, [strjoin(labels, ',') '\n']);
 fclose(fid);
 dlmwrite(filename, stim_x_el, '-append', 'precision', '%.6f', 'delimiter', ',');
