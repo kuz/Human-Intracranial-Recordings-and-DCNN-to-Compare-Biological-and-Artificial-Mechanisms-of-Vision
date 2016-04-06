a315e0bcb7ccb03b451f7585e698f9f916087f88
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-04-06 11:20:33 +0300

    changed frequency data extractors from the LFP data
    Former-commit-id: 01dd7f51a01b251326a464bb6ec52c50ddc91146

--- Intracranial/feature_extractors/extract_meanbeta.sh
+++ Intracranial/feature_extractors/extract_meanbeta.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-matlab -nojvm -nodisplay -nosplash -r "range = [1:12]; extract_meanbeta; exit" < /dev/null > /dev/null &
-matlab -nojvm -nodisplay -nosplash -r "range = [13:24]; extract_meanbeta; exit" < /dev/null > /dev/null &
-matlab -nojvm -nodisplay -nosplash -r "range = [25:36]; extract_meanbeta; exit" < /dev/null > /dev/null &
-matlab -nojvm -nodisplay -nosplash -r "range = [37:48]; extract_meanbeta; exit" < /dev/null > /dev/null &
-matlab -nojvm -nodisplay -nosplash -r "range = [49:60]; extract_meanbeta; exit" < /dev/null > /dev/null &
-matlab -nojvm -nodisplay -nosplash -r "range = [61:72]; extract_meanbeta; exit" < /dev/null > /dev/null &
-matlab -nojvm -nodisplay -nosplash -r "range = [73:84]; extract_meanbeta; exit" < /dev/null > /dev/null &
-matlab -nojvm -nodisplay -nosplash -r "range = [85:100]; extract_meanbeta; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [1:12]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [13:24]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [25:36]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [37:48]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [49:60]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [61:72]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [73:84]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
+matlab -nojvm -nodisplay -nosplash -r "range = [85:100]; bandname = 'beta'; freqlimits = [13 30]; bins=[[13 18]; [19 24]; [25 30]]; extract_meanband; exit" < /dev/null > /dev/null &
