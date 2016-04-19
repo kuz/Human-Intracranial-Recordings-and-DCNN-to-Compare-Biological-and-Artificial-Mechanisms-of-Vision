5aeb49d968b25d370c8d6a687d5ea0b0e81940d3
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-04-19 18:43:14 +0300

    added preprocessing step that finds trials with artifacts and replaces LFP of those trials with zeros, this will be used in feature extractor to mark those trials as invalid and add a poison pull instead of actual values
    Former-commit-id: e174f8f30c2a270433ee12b9fcb7564c5a9ac696

--- Intracranial/preprocess_lfp_artifacts.m
+++ Intracranial/preprocess_lfp_artifacts.m
@@ -1,11 +1,14 @@
 % paramters
 indata = 'LFP_bipolar_noscram_ventral';
-outdata = '';
+outdata = 'LFP_bipolar_noscram_ventral_artif';
+threshold = 10.0;
 
 % load subject list
 listing = dir(['../../Data/Intracranial/Processed/' indata '/*.mat']);
-total_recs = 0;
-buggy_recs = 0;
+affected_trials = 0;
+total_trials = 0;
+drop_probes = 0;
+total_probes = 0;
 
 % process file-by-file
 for sfile = listing'
@@ -13,37 +16,53 @@ for sfile = listing'
     
     % load the data
     load(['../../Data/Intracranial/Processed/' indata '/' sfile.name]);
-    s.probes.mni(isnan(s.probes.mni)) = 0;
 
-    % store the data
-    %save(['../../../Data/Intracranial/Processed/biploar_ventral/' sfile.name], 's');
-
-    % for each electrode and stimulus take mean and std of the signal and then take
-    % mean over stimuli, by doing that we obtain mean mean and mean std for each probe
-    mus = mean(s.data, 3);
-    stds = std(s.data, 0, 3);
-    avg_mus = mean(mus, 1);
-    avg_stds = mean(stds, 1);
-
-    % for each probe count number of abnormal recordings
-    %drop_probes = [];
-    for i = 1:size(mus, 2)
-        bad_trials = sum(abs(mus(:, i) - avg_mus(i)) / avg_stds(i) > 3);
-        if bad_trials > 0
-            disp(bad_trials)
+    % detrend
+    for si = 1:size(s.data, 1)
+        for pi = 1:size(s.data, 2)
+            s.data(si, pi, :) = detrend(squeeze(s.data(si, pi, :)));
         end
-        %total_recs = total_recs + size(mus, 1);
-        %buggy_recs = buggy_recs + sum(abs(mus(:, i) - avg_mus(i)) / avg_stds(i) > 3);
-        %if abs(mus(:, i) - avg_mus(i)) / avg_stds(i) > 5
-        %    %drop_probes = [drop_probes, i]
-        %    buggy_recs = buggy_recs + 1;
-        %end 
     end
-    %disp(length(drop_probes))
-    %disp(total_recs)
-    %disp(buggy_recs)
-
+    
+    %mus = mean(s.data, 3);
+    stds = std(s.data, 0, 3);
+    maxs = max(s.data, [], 3);
+    avg_stds = mean(stds, 1);
+    
+    %
+    % This chunk of code detects and drops individual "trials", this will
+    % result in different number of trials per probe, but that is probably
+    % ok
+    %
+    for i = 1:size(stds, 2)
+        bad_trials = maxs(:, i) > avg_stds(i) * threshold;
+        s.data(bad_trials, i, :) = repmat(0, size(s.data(bad_trials, i, :)));
+    end
+    
+    %
+    % The following chunk of code covers the case when we drop the whole
+    % probes if som % of its recordings have artifacts
+    %
+    
+    %total_trials = total_trials + size(stds, 1);
+    %total_probes = total_probes + size(stds, 2);
+    %
+    %for i = 1:size(stds, 2)
+    %    bad_trials = sum(maxs(:, i) > avg_stds(i) * threshold);
+    %    affected_trials = affected_trials + bad_trials;
+    %    if bad_trials > 0
+    %        percent_bad = bad_trials / size(stds, 1) * 100;
+    %        %disp(['Probe ' num2str(i) ': ' num2str(percent_bad)])
+    %        if percent_bad > 20.0
+    %            drop_probes = drop_probes + 1;
+    %        end
+    %    end
+    %end
+    
+    % store the data
+    save(['../../../Data/Intracranial/Processed/' outdata '/' sfile.name], 's');
+    
     % drop all variables which are relevant to this subject
-    clearvars -except listing indata outdata total_recs buggy_recs
+    clearvars -except listing indata outdata affected_trials total_trials drop_probes total_probes threshold
     
 end
