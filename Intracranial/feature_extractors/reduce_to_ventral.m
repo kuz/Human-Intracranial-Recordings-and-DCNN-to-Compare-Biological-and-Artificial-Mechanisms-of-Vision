33e7192abec7017144ef5bfbbbf6dbdf285306cd
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-06-18 10:53:55 +0300

    (minor updates)
    Former-commit-id: ce70c8d31c62f6510d58a4081cd8b915ac0c6096

--- Intracranial/feature_extractors/reduce_to_ventral.m
+++ Intracranial/feature_extractors/reduce_to_ventral.m
@@ -4,11 +4,12 @@
 
 % imports
 addpath('../lib/mni2name')
+addpath('../lib/nifti')
 
 % parameters
-indata = 'LFP_bipolar_noscram_artif';
-atlas = 'initial';
-outdata = ['LFP_bipolar_noscram_artif_ventral_' atlas];
+indata = 'meangamma_bipolar_noscram_artif';
+atlas = 'brodmann';
+outdata = [indata '_ventral_' atlas];
 if exist(['../../../Data/Intracranial/Processed/' outdata], 'dir') == 7
     disp(['Directory exists: ' outdata ', exiting...'])
     exit()
@@ -16,15 +17,15 @@ end
 mkdir(['../../../Data/Intracranial/Processed/' outdata])
 
 % load atlas
-if atlas == 'initial'
+if strcmp(atlas, 'initial')
     talareich_level = 5;
     areas_of_interest = {'brodmann area 17', 'brodmann area 18', 'brodmann area 19', ...
                          'brodmann area 37', 'brodmann area 20', 'brodmann area 38', ...
                          'brodmann area 28', 'brodmann area 27', 'brodmann area 35'};
-elseif atlas == 'brodmann'
+elseif strcmp(atlas, 'brodmann')
     db = load_nii('lib/mni2name/brodmann.nii');
     areas_of_interest = {'17', '18', '19', '37', '20', '38', '28', '27', '35'};
-elseif atlas == 'aicha'
+elseif strcmp(atlas, 'aicha')
     db = load_nii('lib/mni2name/aicha.nii');
     labels = load('lib/mni2name/aicha.labels.mat');
     areas_of_interest = {}; % TODO
@@ -42,13 +43,13 @@ for sfile = listing'
     
     % use atlas to map probes to areas
     s.probes.mni(isnan(s.probes.mni)) = 0;
-    if atlas == 'initial'
+    if strcmp(atlas, 'initial')
         [~, areas] = mni2name(s.probes.mni);
         nareas = size(areas, 1);
-    elseif atlas == 'brodmann'
+    elseif strcmp(atlas, 'brodmann')
         [~, areas] = mni2name_brodmann(s.probes.mni, db);
         nareas = length(areas);
-    elseif atlas == 'aicha'
+    elseif strcmp(atlas, 'aicha')
         [~, areas] = mni2name_aicha(s.probes.mni, db);
         nareas = length(areas);
     end
@@ -58,11 +59,11 @@ for sfile = listing'
     for i = 1:nareas
         
         % pick contrainer key depending on the atlas in use
-        if atlas == 'initial'
+        if strcmp(atlas, 'initial')
             key = areas{i, talareich_level};
-        elseif atlas == 'brodmann'
+        elseif strcmp(atlas, 'brodmann')
             key = num2str(areas{i});
-        elseif atlas == 'aicha'
+        elseif strcmp(atlas, 'aicha')
             if areas{i} == 0
                 key = '0';
             else
@@ -86,6 +87,6 @@ for sfile = listing'
     save(['../../../Data/Intracranial/Processed/' outdata '/' sfile.name], 's');
     
     % drop all variables which are relevant to this subject
-    clearvars -except listing talareich_level areas_of_interest indata outdata
+    clearvars -except listing talareich_level areas_of_interest indata outdata atlas db labels
     
 end
