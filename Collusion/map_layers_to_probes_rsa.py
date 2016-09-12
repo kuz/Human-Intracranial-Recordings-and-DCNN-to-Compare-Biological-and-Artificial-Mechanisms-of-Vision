61b16ff2ed68bd3f49edf02b809b3f16b29df254
kuz <ilya.kuzovkin@gmail.com>
2016-09-12 18:19:56 +0300

    shuffle cmd option for the brain RDM computation
    Former-commit-id: 8c3a098352e7e088c5e6c71f4ac72407b94d103e

--- Collusion/map_layers_to_probes_rsa.py
+++ Collusion/map_layers_to_probes_rsa.py
@@ -2,7 +2,7 @@ import os
 import glob
 import numpy as np
 import argparse
-from scipy.stats import spearmanr
+from scipy.stats import spearmanr, pearsonr
 from sklearn.preprocessing import MinMaxScaler
 
 # read in command line arguments
@@ -10,20 +10,26 @@ parser = argparse.ArgumentParser(description='Compute RSA matrices for each elec
 parser.add_argument('-i', '--sid', dest='sid', type=int, required=True, help='Subject ID')
 parser.add_argument('-f', '--featureset', dest='featureset', type=str, required=True, help='Directory with brain features (Processed/?)')
 parser.add_argument('-d', '--distance', dest='dist', type=str, required=True, help='The distance metric to use')
+parser.add_argument('-s', '--shuffle', dest='shuffle', type=bool, required=False, default=False, help='Whether to shuffle data for a permutation test')
 args = parser.parse_args()
 sid = int(args.sid)
 featureset = str(args.featureset)
 dist = str(args.dist)
+shuffle = bool(args.shuffle)
+
+suffix = ''
+if shuffle:
+    suffix = '.shuffled'
 
 # load layer similarity matrices including the "layer 0" (pixel space)
 layers = ['pixels', 'conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7', 'fc8']
 dnn_dsm = {}
 for layer in layers:
-    dnn_dsm[layer] = np.loadtxt('../../Data/RSA/%s/numbers/dnn-%s.txt' % (dist, layer))
+    dnn_dsm[layer] = np.loadtxt('../../Data/RSA/%s%s/numbers/dnn-%s.txt' % (dist, suffix, layer))
 
 # load brain response dissimilarity matrices
 brain_dsm = {}
-listing = glob.glob('../../Data/RSA/%s/numbers/brain-%d-*.txt' % (dist, sid))
+listing = glob.glob('../../Data/RSA/%s%s/numbers/brain-%d-*.txt' % (dist, suffix, sid))
 for pid, filename in enumerate(listing):
     brain_dsm[pid] = np.loadtxt(filename)
 
@@ -40,18 +46,21 @@ for lid, layer in enumerate(layers):
         for i in range(nstim):
             # here we can decide what do we use as a score between the two matrices
             # a) number of images that were significantly correlated in both matrices
-            r, p = spearmanr(dnn[i, :], brain[i, :])
-            if p < 0.0001:
-                score += 1
+            #r, p = spearmanr(dnn[i, :], brain[i, :])
+            #if p < 0.00001:
+            #    score += 1
 
             # b) sum of r scores of all significanly correlated images
-            # todo
-        maps[pid, lid] = score
+            r, p = spearmanr(dnn[i, :], brain[i, :])
+            if p < 0.00001:
+                score += r
+        
+        maps[pid, lid] = score / float(nstim)
 
 # replace NaN's with 0
 maps[np.isnan(maps)] = 0.0
 
 # store the scores
 subjects = os.listdir('../../Data/Intracranial/Processed/%s/' % featureset)
-np.savetxt('../../Data/Intracranial/Probe_to_Layer_Maps/rsa_%s_%s/%s.txt' % (dist, featureset, os.path.splitext(subjects[sid])[0]), maps, fmt='%.4f')
+np.savetxt('../../Data/Intracranial/Probe_to_Layer_Maps/rsa_%s%s_%s/%s.txt' % (dist, suffix, featureset, os.path.splitext(subjects[sid])[0]), maps, fmt='%.4f')
 
