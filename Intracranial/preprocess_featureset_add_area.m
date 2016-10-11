4c3684615851a4acf7c18d44e7a06f7beb1c6332
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-10-11 21:29:00 +0300

    Extent the data files by adding area code information into it
    Former-commit-id: e934a19f0ca29f9fbc587d609d80de0fcc83bb13

---
+++ Intracranial/preprocess_featureset_add_area.m
@@ -0,0 +1,29 @@
+% paramters
+indata = 'meangamma_bipolar_noscram_artif_responsive';
+outdata = 'meangamma_bipolar_noscram_artif_responsive_brodmann';
+
+% load libraries
+addpath('lib/mni2name')
+addpath('lib/nifti')
+db = load_nii('lib/mni2name/brodmann.nii');
+
+% load subject list
+listing = dir(['../../Data/Intracranial/Processed/' indata '/*.mat']);
+
+% for each subject
+for si = 1:length(listing)
+    sfile = listing(si);
+    disp(['Processing ' sfile.name])
+    
+    % load the data
+    load(['../../Data/Intracranial/Processed/' indata '/' sfile.name]);
+    
+    % assign areas
+    [~, areas] = mni2name_brodmann(s.probes.mni, db);
+    areas = cell2mat(areas)';
+    s.probes.areas = areas;
+    
+    % store the data
+    save(['../../Data/Intracranial/Processed/' outdata '/' sfile.name], 's');
+    clearvars -except indata outdata db listing
+end
