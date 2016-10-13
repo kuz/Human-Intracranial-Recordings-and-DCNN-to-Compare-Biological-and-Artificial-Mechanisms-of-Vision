212f1fdd0cd40b482812a67b2789ba46a1943be9
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-10-13 16:24:17 +0300

    OO RSA for DNN layers
    Former-commit-id: 3b9d95cecbf2add164ba93b8aaf6e970cc9f724c

--- Collusion/RSA.py
+++ Collusion/RSA.py
@@ -9,8 +9,9 @@ import argparse
 class RSA:
     __metaclass__ = ABCMeta
 
-    #: The project data directory
+    #: Paths
     DATADIR = '../../Data'
+    CODEDIR = '../../Repository'
 
     #: The distance metric
     distance = None
@@ -59,9 +60,11 @@ class RSA:
     def load_dsm(self):
         pass
 
+    @abstractmethod
+    def plot_dsm(self):
+        pass
 
 class RSAPixel(RSA):
-    
     representation = None
     dsm = None
 
@@ -81,8 +84,49 @@ class RSAPixel(RSA):
     def load_dsm(self):
         self.dsm = np.loadtxt('%s/RSA/%s/numbers/dnn-pixels.txt' % (self.DATADIR, self.distance))
 
+    def plot_dsm(self):
+        #plt.figure();
+        #plt.imshow(self.dsm);
+        #plt.colorbar();
+        #plt.savefig('%s/RSA/%s/plots/dnn-pixels.png' % (self.DATADIR, self.distance));
+        #plt.clf();
+
+
 class RSADNN(RSA):
-    pass
+    representation = None
+    dsm = {}
+    layers = None
+
+    def __init__(self, distance, np_activation_data):
+        RSA.__init__(self, distance)
+
+        self.layers = os.listdir('%s/DNN/activations/%s' % (self.CODEDIR, np_activation_data))
+        self.representation = {}
+        for layer in self.layers:
+            self.representation[layer] = np.load('%s/DNN/activations/%s/%s/activations.npy' % (self.CODEDIR, np_activation_data, layer))
+            self.representation[layer] = self.representation[layer][self.reorder_dnn_to_categories]
+
+    def compute_dsm(self):
+        for layer in self.layers:
+            print 'Computing the matrix for layer %s...' % layer
+            self.dsm[layer] = scipydist.squareform(scipydist.pdist(self.representation[layer], self.distance))
+
+    def save_dsm(self):
+        for layer in self.layers:
+            np.savetxt('%s/RSA/%s/numbers/dnn-%s.txt' % (self.DATADIR, self.distance, layer), self.dsm[layer], fmt='%.6f')
+
+    def load_dsm(self):
+        for layer in self.layers:
+            self.dsm[layer] = np.loadtxt('%s/RSA/%s/numbers/dnn-%s.txt' % (self.DATADIR, self.distance, layer))
+
+    def plot_dsm(self):
+        for layer in self.layers:
+            #plt.figure();
+            #plt.imshow(self.dsm);
+            #plt.colorbar();
+            #plt.savefig('%s/RSA/%s/plots/dnn-%s.png' % (self.DATADIR, dist, layer));
+            #plt.clf();
+            pass
 
 
 class RSABrain(RSA):
@@ -94,15 +138,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compute RDM matrices')
     parser.add_argument('-s', '--source', dest='source', type=str, required=True, help='The data source: pixels, dnn or brain')
     parser.add_argument('-d', '--distance', dest='distance', type=str, required=True, help='The distance metric to use')
+    parser.add_argument('-a', '--activations', dest='np_activation_data', type=str, required=False, help='DNN activations for DNN RDMs')
     args = parser.parse_args()
     source = str(args.source)
     distance = str(args.distance)
+    np_activation_data = str(args.np_activation_data)
 
     if source == 'pixels':
         rsa = RSAPixel(distance)
         rsa.compute_dsm()
         rsa.save_dsm()
     elif source == 'dnn':
+        np_activation_data
         pass
     elif source == 'brain':
         pass
