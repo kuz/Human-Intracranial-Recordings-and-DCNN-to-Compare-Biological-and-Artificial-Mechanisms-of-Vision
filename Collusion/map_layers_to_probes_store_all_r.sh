99b1a009b1c33b63e2eeee182de06e20b8391661
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-11-05 22:41:37 +0200

    Put procedural linear mapper back, as Parallel does not work with class instances
    Former-commit-id: 881e4b58682b98b5de50062ec1922d44edd9d470

---
+++ Collusion/map_layers_to_probes_store_all_r.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ~/Python/bin/activate
+FEATURESET=meangamma_bipolar_noscram_artif_responsive
+nfiles=$(ls -l ../../Data/Intracranial/Processed/$FEATURESET/*.mat | wc -l)
+for i in $(seq 1 $nfiles)
+do
+    let i=i-1
+    srun -N 1 --partition=long --cpus-per-task=4 --mem=15000 --exclude idu[38-41] python map_layers_to_probes_store_all_r.py --sid $i --activations numpy.reference --featureset $FEATURESET &
+    sleep 10
+done
