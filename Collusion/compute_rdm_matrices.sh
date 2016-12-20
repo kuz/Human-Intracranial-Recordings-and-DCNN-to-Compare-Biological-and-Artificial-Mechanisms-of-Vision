c05948c372658cc6273dbf7cae85e34c1b768f1e
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-12-20 17:46:21 +0200

    RDM matrix computation script now is called with cmd parameters
    Former-commit-id: e74dcd5ad5d9b26510dfedd2b0513d56d1b28a95

--- Collusion/compute_rdm_matrices.sh
+++ Collusion/compute_rdm_matrices.sh
@@ -2,21 +2,30 @@
 
 # parameters
 source ~/Python/bin/activate
-FEATURESET=meanalpha_LFP_bipolar_noscram_artif_brodmann_w150_alpha_resppositive
-DISTANCE=euclidean
+
+FEATURESET=$1
+DISTANCE=$1
+if [ -z "$FEATURESET" ]; then
+    echo 'Error: FEATURESET is not specified'
+    exit
+fi
+if [ -z "$DISTANCE" ]; then
+    echo 'Error: DISTANCE is not specified'
+    exit
+fi
 
 # pixels
-srun -N 1 --partition=long --cpus-per-task=1 --mem=2000 -t 24:00:00 python RDM.py -t pixels -d $DISTANCE -f $FEATURESET &
+srun -N 1 --partition=long --cpus-per-task=1 --mem=2000 --exclude idu[38-41] python RDM.py -t pixels -d $DISTANCE -f $FEATURESET &
 
 # dnn
-srun -N 1 --partition=long --cpus-per-task=1 --mem=6000 -t 24:00:00 python RDM.py -t dnn -d $DISTANCE -f $FEATURESET -a numpy.reference &
+srun -N 1 --partition=long --cpus-per-task=1 --mem=6000 --exclude idu[38-41] python RDM.py -t dnn -d $DISTANCE -f $FEATURESET -a numpy.reference &
 
 # brain
 nfiles=$(ls -l ../../Data/Intracranial/Processed/$FEATURESET/*.mat | wc -l)
 for i in $(seq 1 $nfiles)
 do
     let i=i-1
-    srun -N 1 --partition=long --cpus-per-task=1 --mem=2000 -t 24:00:00 python RDM.py -t brain -d $DISTANCE -i $i -f $FEATURESET &
+    srun -N 1 --partition=long --cpus-per-task=1 --mem=2000 --exclude idu[38-41] python RDM.py -t brain -d $DISTANCE -i $i -f $FEATURESET &
     sleep 2
 done
 
