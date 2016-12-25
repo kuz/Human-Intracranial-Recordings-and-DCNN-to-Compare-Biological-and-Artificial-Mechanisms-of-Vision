ab55fb7583db10fb08aca57cdeff547d60b726c2
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2016-12-25 19:11:53 +0200

    added parameters to RSA scoring script
    Former-commit-id: d57045e9dbc7c2b6ee5df6aa7663f59648e1a6ba

--- Collusion/compute_scores_on_rdms.sh
+++ Collusion/compute_scores_on_rdms.sh
@@ -3,10 +3,27 @@
 export LD_LIBRARY_PATH=/gpfs/hpchome/a72073/Python/lib/:/usr/lib:/usr/local/lib:/usr/lib64:/usr/local/lib64:/gpfs/hpchome/a72073/Software/lib
 source ~/Python/bin/activate
 
-FEATURESET=meanhighgamma_LFP_bipolar_noscram_artif_brodmann_w150_highgamma_resppositive
-DISTANCE=euclidean
-ONWHAT=matrix
-THRESHOLD=1.0
+FEATURESET=$1
+DISTANCE=$2
+ONWHAT=$3
+THRESHOLD=$4
+if [ -z "$FEATURESET" ]; then
+    echo 'Error: FEATURESET is not specified'
+    exit
+fi
+if [ -z "$DISTANCE" ]; then
+    echo 'Error: DISTANCE is not specified'
+    exit
+fi
+if [ -z "$ONWHAT" ]; then
+    echo 'Error: ONWHAT is not specified'
+    exit
+fi
+if [ -z "$THRESHOLD" ]; then
+    echo 'Error: THRESHOLD is not specified'
+    exit
+fi
+
 nfiles=$(ls -l ../../Data/Intracranial/Processed/$FEATURESET/*.mat | wc -l)
 for i in $(seq 1 $nfiles)
 do
